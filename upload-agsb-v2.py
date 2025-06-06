c0a605582fa5e22b3cd9c84b3752f7536c84bb05
joachim508 <150675441+joachim508@users.noreply.github.com>
2025-06-06 22:41:37 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -33,9 +33,9 @@ CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的�
 # ====== 全局可配置参数（可直接在此处修改） ======
 USER_NAME = "joachim58"         # 用户名
 UUID = "484c676d-f5e7-4231-914f-2dfc8362cf52"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
+PORT = 59999                   # Vmess端口，留空或0则自动生成
 DOMAIN =  "597498658.xyz"                  # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiMzZlODQzOWZhMzlkOTkyYmVlZGVjNWNlMWE1M2ZlNDAiLCJ0IjoiYmViODIyNjItOTliYi00MDZlLTkxYTUtYjUxY2RlYmQ3YjhlIiwicyI6IlpqZ3hZalkwT0dNdE16ZG1NeTAwWW1WbUxUZ3dOR1F0T0dRME1ESTFaR0UzWXpNMiJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+CF_TOKEN = "eyJhIjoiMzZlODQzOWZhMzlkOTkyYmVlZGVjNWNlMWE1M2ZlNDAiLCJ0IjoiZWU4NjM2ZmEtZThlMS00OWRjLTk3MWQtNjE5M2UyOTFlYzAwIiwicyI6IlpqZzFNalprWW1VdE1XUm1OQzAwTnpjNUxXRTBNV010TjJGak9XVmxZelUwT1RNMCJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
