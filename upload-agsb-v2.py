0c68cb0535a264c523633304c9a901773770a187
joachim508 <150675441+joachim508@users.noreply.github.com>
2025-06-06 10:30:56 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -34,7 +34,7 @@ CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的�
 USER_NAME = "joachim58"         # 用户名
 UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
 PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN =                    # 域名，留空则自动获取
+DOMAIN =  "free.testlrj.ddns-ip.net"                  # 域名，留空则自动获取
 CF_TOKEN = "eyJhIjoiMzZlODQzOWZhMzlkOTkyYmVlZGVjNWNlMWE1M2ZlNDAiLCJ0IjoiYmViODIyNjItOTliYi00MDZlLTkxYTUtYjUxY2RlYmQ3YjhlIiwicyI6IlpqZ3hZalkwT0dNdE16ZG1NeTAwWW1WbUxUZ3dOR1F0T0dRME1ESTFaR0UzWXpNMiJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
